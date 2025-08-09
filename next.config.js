116f4423122defec79b82389c3765a1c2711ffaf
Pallav Bhardwaj <pallavbh23@gmail.com>
2025-08-09 16:02:43 +0530

    fixed build issues and vulnerabilities

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
-const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ });
-
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: { providerImportSource: '@mdx-js/react' },
+});
 module.exports = withMDX({
-  pageExtensions: ['ts', 'tsx', 'mdx'],
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'mdx'],
   images: {
-    // allow external images you actually use
     remotePatterns: [
-      { protocol: 'https', hostname: 'github.com' },                 // avatar
-      { protocol: 'https', hostname: 'leetcard.jacoblin.cool' },     // if using that LeetCode card
-      { protocol: 'https', hostname: 'leetcode-stats.vercel.app' }   // if using this LeetCode card
+      { protocol: 'https', hostname: 'github.com' },
+      { protocol: 'https', hostname: 'leetcard.jacoblin.cool' },
+      { protocol: 'https', hostname: 'leetcode-stats.vercel.app' }
     ],
   },
 });
